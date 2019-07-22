03fe2c3d2e67c2883048e47689b2804902bffba0
Albert Tedja <albert@siliconaxon.com>
2019-07-22 05:15:47 +0000

    Using defer instead to Cleanup

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ func main() {
 	done := make(chan struct{})
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	defer Cleanup()
+
 	go func() {
 		<-c
-		log.Println("Stopping services.")
-		Cleanup()
 		close(done)
 	}()
 
@@ -28,5 +28,7 @@ func main() {
 }
 
 func Cleanup() {
+	log.Println("Stopping services.")
+
 	log.Println("Shutdown.")
 }
